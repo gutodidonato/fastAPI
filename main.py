7765dc2bec40dc3f389eabd989bf8006a284336f
Guto <gutodidonato@gmail.com>
2024-04-22 21:53:11 -0300

    teste

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
-restaurante = {
-    1: {"nome": "Restaurante da tia Teteia", "status": "ativo"},
-    2: {"nome": "Cantinho do Sabor", "status": "ativo"},
-    3: {"nome": "Sabor Familiar", "status": "ativo"},
-    4: {"nome": "Rancho da Vovó", "status": "ativo"},
-    5: {"nome": "Sabor Caseiro", "status": "ativo"},
-}
+restaurantes = [
+    {"nome": "Restaurante da tia Teteia", "status": "ativo"},
+    {"nome": "Cantinho do Sabor", "status": "ativo"},
+    {"nome": "Sabor Familiar", "status": "ativo"},
+    {"nome": "Rancho da Vovó", "status": "ativo"},
+    {"nome": "Sabor Caseiro", "status": "ativo"},
+]
 
 
-@app.get("/restaurante/{id_restaurante}")
-def pegar_restaurante(id_restaurante: int):
-    if id_restaurante in restaurante:
-        return restaurante[id_restaurante]
-    else:
-        return {"erro": f"Não existe um Restaurante com o ID {id_restaurante}"}
+@app.get("/restaurantes/")
+async def listar_restaurantes():
+    return restaurantes
 
 
 if __name__ == "__main__":
