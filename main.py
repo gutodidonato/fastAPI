f797a173158c118f633a0ac20823261997baf28c
Guto <gutodidonato@gmail.com>
2024-04-22 22:03:46 -0300

    app

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from os import getenv
 import uvicorn
 
-app = FastAPI()
-
-restaurantes = [
-    {"nome": "Restaurante da tia Teteia", "status": "ativo"},
-    {"nome": "Cantinho do Sabor", "status": "ativo"},
-    {"nome": "Sabor Familiar", "status": "ativo"},
-    {"nome": "Rancho da Vovó", "status": "ativo"},
-    {"nome": "Sabor Caseiro", "status": "ativo"},
-]
-
-
-@app.get("/restaurantes/")
-async def listar_restaurantes():
-    return restaurantes
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, port=8080)
+    port = int(getenv("PORT", 8000))
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
